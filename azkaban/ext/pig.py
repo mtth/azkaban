cf75a14d97523eabbd4ae13d1aac3f9fb9a21e67
Matthieu Monsch <monsch@alum.mit.edu>
2014-02-12 08:31:02 -0800

    added `--jar` option and changed `OPTION` to `--option`.

--- azkaban/ext/pig.py
+++ azkaban/ext/pig.py
@@ -4,18 +4,26 @@
 """AzkabanPig: an extension for pig scripts to Azkaban CLI.
 
 Usage:
-  azkabanpig [-p PROJECT] [-t TYPE] (-u URL | -a ALIAS) PATH [OPTION ...]
+  azkabanpig [-p PROJECT] [-t TYPE] (-u URL | -a ALIAS) PATH ...
+             [-j JAR] ... [-o OPTION] ...
   azkabanpig -h | --help
 
 Arguments:
-  PATH                          Path to pig script.
-  OPTION                        Azkaban option. Should be of the form
-                                key=value. E.g. 'param.foo=bar' will substitute
-                                parameter '$foo' with 'bar' in the pig script.
+  PATH                          Path to pig script. If more than one path is
+                                specified, they will be run in order. Note that
+                                all the pig jobs will share the same Azkaban
+                                options; for more flexibility, you can use the
+                                Azkaban CLI directly.
 
 Options:
   -a ALIAS --alias=ALIAS        Cf. `azkaban --help`.
   -h --help                     Show this message and exit.
+  -j JAR --jar=JAR              Path to jar file. It will be available on the
+                                class path when the pig script is run.
+  -o OPTION --option=OPTION     Azkaban option. Should be of the form
+                                key=value. E.g. '-o param.foo=bar' will
+                                substitute parameter '$foo' with 'bar' in the
+                                pig script.
   -p PROJECT --project=PROJECT  Project name under which to run the pig script
                                 [default: pig_${user}].
   -t TYPE --type=TYPE           Pig job type used [default: pig].
@@ -25,6 +33,7 @@ Options:
 
 from docopt import docopt
 from getpass import getuser
+from os import sep
 from os.path import abspath, basename
 from string import Template
 from sys import exit, stderr, stdout
@@ -35,53 +44,73 @@ from ..util import AzkabanError, temppath
 
 class PigProject(Project):
 
-  """Project to run a single pig job from the command line.
+  """Project to run pig jobs from the command line.
 
-  :param path: path to a pig script
+  :param name: project name used
+  :param paths: paths to pig scripts, these will be run in order
   :param pig_type: pig job type used
-  :param options: options forwarded to pig script
+  :param jars: jars to include, these will also be added to the classpath
+  :param options: options forwarded to pig scripts
 
   """
 
-  def __init__(self, name, path, pig_type='pig', options=None):
+  def __init__(self, name, paths, pig_type='pig', jars=None, options=None):
     super(PigProject, self).__init__(name, register=False)
-    job = PigJob(
-      abspath(path),
+    jars = jars or []
+    opts = [
       {
         'type': pig_type,
         'user.to.proxy': getuser(),
+        'pig.additional.jars': ','.join(abspath(j).lstrip(sep) for j in jars),
       },
-      options or {},
-    )
-    self.add_job(basename(path), job)
+      options or {}
+    ]
+    for jar in jars:
+      self.add_file(abspath(jar))
+    for path, dep in zip(paths, [None] + paths):
+      dep_opts = {'dependencies': dep} if dep else {}
+      self.add_job(basename(path), PigJob(abspath(path), dep_opts, *opts))
 
 
 def main():
   """AzkabanPig entry point."""
   args = docopt(__doc__)
-  path = args['PATH']
-  job_name = basename(path)
   project_name = Template(args['--project']).substitute(user=getuser())
+  paths = args['PATH']
   try:
     try:
-      job_options = dict(opt.split('=', 1) for opt in args['OPTION'])
+      job_options = dict(opt.split('=', 1) for opt in args['--option'])
     except ValueError:
       raise AzkabanError('Invalid options: %r' % (' '.join(args['OPTION']), ))
-    project = PigProject(project_name, path, args['--type'], job_options)
+    project = PigProject(
+      name=project_name,
+      paths=paths,
+      pig_type=args['--type'],
+      jars=args['--jar'],
+      options=job_options,
+    )
     session = project.get_session(url=args['--url'], alias=args['--alias'])
+    url = session['url']
+    session_id = session['session_id']
     with temppath() as tpath:
       project.build(tpath)
       try:
-        project.upload(tpath, session['url'], session['session_id'])
+        project.upload(tpath, url, session_id)
       except AzkabanError:
-        project.create(project.name, session['url'], session['session_id'])
-        project.upload(tpath, session['url'], session['session_id'])
-    res = project.run(job_name, session['url'], session['session_id'])
+        project.create(project.name, url, session_id)
+        project.upload(tpath, url, session_id)
+    res = project.run(basename(paths[-1]), url, session_id)
     exec_id = res['execid']
-    stdout.write(
-      'Pig job running at %s/executor?execid=%s&job=%s\n'
-      % (session['url'], exec_id, job_name)
-    )
+    if len(paths) == 1:
+      stdout.write(
+        'Pig job running at %s/executor?execid=%s&job=%s\n'
+        % (session['url'], exec_id, basename(paths[0]))
+      )
+    else:
+      stdout.write(
+        'Pig jobs flow running at %s/executor?execid=%s\n'
+        % (session['url'], exec_id)
+      )
   except AzkabanError as err:
     stderr.write('%s\n' % (err, ))
     exit(1)
