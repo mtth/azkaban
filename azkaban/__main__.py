df1f4a1a19e1b9c990798c87dc4a6f7016770c79
Matthieu Monsch <monsch@alum.mit.edu>
2014-02-15 18:37:19 -0800

    main entry point bug fixes

--- azkaban/__main__.py
+++ azkaban/__main__.py
@@ -75,20 +75,34 @@ Azkaban CLI returns with exit code 1 if an error occurred and 0 otherwise.
 
 from azkaban import __version__
 from azkaban.project import Project
-from azkaban.util import AzkabanError, Session, human_readable, temppath
+from azkaban.session import Session
+from azkaban.util import AzkabanError, Config, human_readable, temppath
 from docopt import docopt
-from os.path import getsize, relpath
+from os.path import exists, getsize, relpath
 from sys import exit, stdout, stderr
 
 
-def resolve(project):
+def get_project(project, strict=False):
   """Resolve project from CLI argument.
 
   :param project: `--project` argument
+  :param strict: only accept registered projects
 
   """
-  parts = project.rsplit(':', 1)
-  return Project.load_from_script(*parts)
+  parts = (project or '').rsplit(':', 1)
+  if len(parts) == 1:
+    if exists(parts[0]):
+      project = Project.load_from_script(parts[0])
+    elif not strict:
+      project = Project(name, register=False)
+    else:
+      raise AzkabanError(
+        'This command requires a registered project as `--project` option.\n'
+        'Specify an existing project configuration script.'
+      )
+  else:
+    project = Project.load_from_script(*parts)
+  return project
 
 def get_session(url=None, alias=None):
   """Get session from CLI options.
@@ -100,6 +114,7 @@ def get_session(url=None, alias=None):
   if url:
     session = Session(url)
   else:
+    config = Config()
     alias = alias or config.get_default_option('azkaban', 'alias')
     session = Session(**config.resolve_alias(alias))
   return session
@@ -107,16 +122,15 @@ def get_session(url=None, alias=None):
 def main():
   """Command line argument parser."""
   args = docopt(__doc__, version=__version__)
-  config = Config()
   try:
     if args['build']:
-      project = resolve(args['--project'])
-      if args['--zip']:
-        path = args['--zip']
-        project.build(path, overwrite=args['--overwrite'])
+      project = get_project(args['--project'], strict=True)
+      if args['ZIP']:
+        path = args['ZIP']
+        project.build(path, overwrite=args['--force'])
         stdout.write(
           'Project successfully built and saved as %r (size: %s).\n'
-          % (path, human_readable(size))
+          % (path, human_readable(getsize(path)))
         )
       else:
         with temppath() as path:
@@ -130,16 +144,17 @@ def main():
             % (
               project,
               res['projectId'],
-              human_readable(get_size(path)),
+              human_readable(getsize(path)),
               res['version'],
               session.url,
               project,
             )
+          )
     elif args['create']:
       session = get_session(url=args['--url'], alias=args['--alias'])
       project = raw_input('Project name: ').strip()
-      description = raw_input('Project description [%s]: ' % (name, )) or name
-      session.create_project(project, description)
+      description = raw_input('Project description [%s]: ' % (project, ))
+      session.create_project(project, description.strip() or project)
       stdout.write(
         'Project %s successfully created.\n'
         'Details at %s/manager?project=%s\n'
@@ -149,26 +164,22 @@ def main():
       session = get_session(url=args['--url'], alias=args['--alias'])
       project = raw_input('Project name: ')
       session.delete_project(project)
-      stdout.write('Project %s successfully deleted.\n' % (name, ))
+      stdout.write('Project %s successfully deleted.\n' % (project, ))
     elif args['list']:
-      project = resolve(args['--project'])
-      if args['--files']:
+      project = get_project(args['--project'], strict=True)
+      if args['--static']:
         for path in project._files:
           stdout.write('%s\n' % (relpath(path), ))
       else:
         for name in project.jobs:
           stdout.write('%s\n' % (name, ))
     elif args['run']:
+      project = get_project(args['--project'])
       flow = args['FLOW']
       jobs = args['JOB']
-      if not project:
-        if name:
-          project = EmptyProject(name)
-        else:
-          project = Project.load_from_script(args['--script'])
-      session = Session(url=args['--url'], alias=args['--alias'])
-      res = project.run(
-        session=session,
+      session = get_session(url=args['--url'], alias=args['--alias'])
+      res = session.run_workflow(
+        project=project,
         flow=flow,
         jobs=jobs,
         block=args['--block'],
@@ -178,63 +189,42 @@ def main():
       stdout.write(
         'Flow %s successfully submitted (execution id: %s%s).\n'
         'Details at %s/executor?execid=%s\n'
-        % (flow, exec_id, job_names, session['url'], exec_id)
+        % (flow, exec_id, job_names, session.url, exec_id)
       )
     elif args['upload']:
-      path = args['--zip']
-      if not project:
-        if path:
-          if name:
-            project = EmptyProject(name)
-          else:
-            raise AzkabanError('Unspecified project name. Use -p option.')
-        else:
-          project = Project.load_from_script(args['--script'], name)
+      path = args['ZIP']
+      project = get_project(args['--project'])
       session = get_session(url=args['--url'], alias=args['--alias'])
-      with temppath() as tpath:
-        if not path:
-          path = tpath
-          project.build(path)
-        size = getsize(path)
+      while True:
         try:
-          res = project.upload(
-            path,
-            url=session['url'],
-            session_id=session['session_id'],
-          )
+          res = session.upload_project(project, path)
         except AzkabanError as err:
           if args['--create']:
-            project.create(
-              description=project.name,
-              url=session['url'],
-              session_id=session['session_id'],
-            )
-            res = project.upload(
-              path,
-              url=session['url'],
-              session_id=session['session_id'],
-            )
+            session.create_project(project, project)
           else:
             raise err
+        else:
+          break
       stdout.write(
         'Project %s successfully uploaded (id: %s, size: %s, version: %s).\n'
         'Details at %s/manager?project=%s\n'
         % (
-          project.name,
+          project,
           res['projectId'],
-          human_readable(size),
+          human_readable(getsize(path)),
           res['version'],
-          session['url'],
-          project.name
+          session.url,
+          project,
         )
       )
     elif args['view']:
-      project = project or Project.load_from_script(args['--script'], name)
+      project = get_project(args['--project'])
       job_name = args['JOB'][0]
       if job_name in project.jobs:
-        pretty_print(project.jobs[job_name])
+        for option, value in sorted(project.jobs[job_name].items()):
+          stdout.write('%s=%s\n' % (option, value))
       else:
-        raise AzkabanError('missing job %r' % (job_name, ))
+        raise AzkabanError('Job %r not found.' % (job_name, ))
   except AzkabanError as err:
     stderr.write('%s\n' % (err, ))
     exit(1)
